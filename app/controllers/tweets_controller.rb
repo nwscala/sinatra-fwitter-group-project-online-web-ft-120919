079907051384cdeaf13cc8e40580d7b4365ae818
nwscala <nwscala@gmail.com>
2020-02-07 20:52:08 -0500

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,76 @@
 class TweetsController < ApplicationController
+    get '/tweets' do
+        # binding.pry
+        if logged_in?
+            @user = current_user
+            @tweets = Tweet.all
+            erb :'/tweets/tweets'
+        else
+            redirect to "/login"
+        end
+    end
 
+    get '/tweets/new' do
+        # binding.pry 
+        if logged_in?
+            @user = current_user
+            erb :'/tweets/new'
+        else
+            redirect to "/login"
+        end
+    end
 
+    post '/tweets' do
+        if params[:content] == ""
+            redirect to "/tweets/new"
+        else
+            @tweet = Tweet.create(:content => params[:content], :user_id => session[:user_id])
+        end
+    end
+
+    get '/tweets/:id' do
+        if logged_in?
+            @tweet = Tweet.find_by(:user_id => params[:id])
+            erb :'/tweets/show'
+        else
+            redirect to '/login'
+        end
+    end
+
+    get '/tweets/:id/edit' do 
+        if logged_in?
+            @tweet = Tweet.find_by(:user_id => params[:id])
+            if @tweet.user_id == session[:user_id]
+                erb :'/tweets/edit'
+            else
+                redirect to '/tweets'
+            end
+        else
+            redirect to '/login'
+        end
+    end
+
+    patch '/tweets/:id' do
+        @tweet = Tweet.find(params[:id])
+        if params[:content] == ""
+            redirect to "/tweets/#{params[:id]}/edit"
+        else
+            @tweet.update(:content => params[:content])
+            redirect to "/tweets/#{params[:id]}"
+        end
+    end
+
+    delete '/tweets/:id' do
+        @tweet = Tweet.find(params[:id])
+        if logged_in?
+            if @tweet.user_id == session[:user_id]
+                @tweet.destroy
+                redirect to "/tweets"
+            else
+                redirect to "/tweets"
+            end
+        else
+            redirect to '/login'
+        end
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
   has_secure_password
   has_many :tweets
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug_to_find)
+    User.all.find do |user|
+      user.slug = slug_to_find
+    end
+  end
 end
